c73f64ce89fa648c35293c625ab0dad88a907c7a
Luís Martinho <lmartinho@gmail.com>
2013-02-16 09:21:45 +0000

    added example 2-2

---
+++ introduction/example-2-2.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'jade');
+app.set('view options', { layout: true});
+app.set('views', __dirname + '/views');
+
+app.get('/stooges/:name?', function(req, res, next) {
+	var name = req.params.name;
+	switch(name ? name.toLowerCase() : '') {
+		case 'larry':
+		case 'curly':
+		case 'moe':
+			res.render('stooges', {stooge: name});
+			break;
+		default:
+			next();
+	}
+});
+
+app.get('/stooges/*?', function(req, res) {
+	res.send('no stooges listed');
+});
+
+app.get('/?', function(req, res) {
+	res.render('index');
+})
+
+var port = 8080;
+app.listen(port);
+console.log('Listening on port ' + port);
